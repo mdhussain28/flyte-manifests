0418071280bc7ad2c356a643a77518a7aeaaf4b9
Mohommed Hussain <101031310+mdhussain28@users.noreply.github.com>
2025-08-05 13:29:56 +0530

    Create reference_lp.py

---
+++ reference_lp.py
@@ -0,0 +1,18 @@
+from flytekit import workflow
+from flytekit import reference_launch_plan
+
+# Reference the launch plan already registered in Flyte
+@reference_launch_plan(
+    project="my-project",
+    domain="development",
+    name="default_launchplan.hello_workflow",
+    version="v2",
+)
+def hello_lp(name: str) -> str:
+    """Reference to default launch plan of hello_workflow"""
+    ...
+
+# Workflow that calls the referenced launch plan
+@workflow
+def call_hello_lp() -> str:
+    return hello_lp(name="Referenced User")
