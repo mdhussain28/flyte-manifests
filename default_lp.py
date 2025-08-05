c3bee99059c823be6c6d46edc07a0c02d5e144b5
Mohommed Hussain <101031310+mdhussain28@users.noreply.github.com>
2025-08-05 12:55:30 +0530

    Create default_lp.py

---
+++ default_lp.py
@@ -0,0 +1,9 @@
+from flytekit import task, workflow
+
+@task
+def say_hello(name: str) -> str:
+    return f"Hello, {name}!"
+
+@workflow
+def hello_workflow(name: str = "Flyte User") -> str:
+    return say_hello(name=name)
