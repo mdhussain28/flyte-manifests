9a13a68ee7ecbd27b86fa8c5f6f0dc9558dbc5a3
Mohommed Hussain <101031310+mdhussain28@users.noreply.github.com>
2025-08-06 12:37:35 +0530

    Update reference_lp.py

--- reference_lp.py
+++ reference_lp.py
@@ -5,8 +5,8 @@ from flytekit import reference_launch_plan
 @reference_launch_plan(
     project="my-project",
     domain="development",
-    name="default_launchplan.hello_workflow",
-    version="v2",
+    name="default_lp.hello_workflow",
+    version="v1",
 )
 def hello_lp(name: str) -> str:
     """Reference to default launch plan of hello_workflow"""
