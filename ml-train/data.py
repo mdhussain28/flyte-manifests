ee0ec01c6ade2846a3467802b79046f3879bd8a9
Mohommed Hussain <101031310+mdhussain28@users.noreply.github.com>
2025-08-14 13:10:56 +0530

    Update data.py

--- ml-train/data.py
+++ ml-train/data.py
@@ -2,7 +2,7 @@ from flytekit import task
 from src.core.core import generate_synthetic_data
 import pandas as pd
 
-@task
+@task(container_image="eyesoncloud/ml-train:v1", requests=Resources(cpu="1", mem="2Gi"), limits=Resources(cpu="1", mem="3Gi"))
 def load_data(n_samples: int = 1000, n_features: int = 12) -> tuple[pd.DataFrame, pd.DataFrame]:
     X, y = generate_synthetic_data(n_samples, n_features)
     return X, pd.DataFrame(y, columns=["label"])
