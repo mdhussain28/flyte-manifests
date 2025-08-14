e600b6efcdd51e49de844c1fdd66bf8b449a6d60
Mohommed Hussain <101031310+mdhussain28@users.noreply.github.com>
2025-08-14 13:23:04 +0530

    Update linear-regression.py

--- ml-train/linear-regression.py
+++ ml-train/linear-regression.py
@@ -1,11 +1,11 @@
-from flytekit import workflow, Resources
+from flytekit import workflow
 import pandas as pd
 from src.tasks.data import load_data
 from src.tasks.train import train_model
 from src.tasks.predict import predict
 from src.orchestration.tune import tune_hyperparameters
 
-@workflow(container_image="eyesoncloud/ml-train:v1", requests=Resources(cpu="1", mem="2Gi"), limits=Resources(cpu="1", mem="3Gi"))
+@workflow
 def ml_workflow(n_samples: int = 1000, n_features: int = 12) -> pd.DataFrame:
     best_params = tune_hyperparameters(n_trials=20)
     X, y = load_data(n_samples=n_samples, n_features=n_features)
